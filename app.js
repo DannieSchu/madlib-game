4ed818c11c92232eebc0893b06aa9ee37fe153f0
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-12 00:48:33 -0800

    toggle element visibility on click

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 // Noun
+// Save user input for "noun" as four different variables
 const inputNoun1 = document.getElementById("input-noun");
 const inputNoun2 = document.getElementById("input-noun");
 const inputNoun3 = document.getElementById("input-noun");
 const inputNoun4 = document.getElementById("input-noun");
 
-// const noun = document.getElementById("noun");
-
+// Save four different variables containing instances of the same noun
 const noun1 = document.getElementById("noun-1");
 const noun2 = document.getElementById("noun-2");
 const noun3 = document.getElementById("noun-3");
@@ -14,60 +14,57 @@ const noun4 = document.getElementById("noun-4");
 
 
 // Adjective 1
-//get input element that contains user input
 const inputAdjective1 = document.getElementById("input-adjective-1");
-
-//get place where we want to put new adjective
 const adjective1 = document.getElementById("adjective-1");
 
 // Royalty
 const inputRoyalty = document.getElementById("input-royalty");
-
 const royalty = document.getElementById("royalty");
 
 // Verb 1
 const inputVerb1 = document.getElementById("input-verb-1");
-
 const verb1 = document.getElementById("verb-1");
 
 // Body part
 const inputBodyPart = document.getElementById("input-body-part");
-
 const bodyPart = document.getElementById("body-part");
 
 // Preposition
 const inputPreposition = document.getElementById("input-preposition");
-
 const preposition = document.getElementById("preposition");
 
 // Adjective 2
 const inputAdjective2 = document.getElementById("input-adjective-2");
-
 const adjective2 = document.getElementById("adjective-2");
 
 // Verb 2
 const inputVerb2 = document.getElementById("input-verb-2");
-
 const verb2 = document.getElementById("verb-2");
 
 // Adjective 3
 const inputAdjective3 = document.getElementById("input-adjective-3");
-
 const adjective3 = document.getElementById("adjective-3");
 
+// Main box (form)
+const mainBox = document.getElementById("main-box");
+
+// Submit 
+const submit = document.getElementById("submit");
+
+// h2 Ready to play?
+const ready = document.getElementById("ready");
 
 // Story box
-// const storyBox = document.getElementById("story-box");
-// storyBox.style.display = "hidden";
+const storyBox = document.getElementById("story-box");
 
+// Photo
+const photo = document.getElementById("photo");
 
-// Get button
-const button = document.getElementById("get-button");
-
+// Button
+const button = document.getElementById("button");
 
 const changeValue = () => {
 // When user enters value, the output word should equal the input
-
     noun1.textContent = inputNoun1.value;
     noun2.textContent = inputNoun2.value;
     noun3.textContent = inputNoun3.value;
@@ -89,10 +86,14 @@ const changeValue = () => {
     
     adjective3.textContent = inputAdjective3.value;
 
+// Story and photo become visible
+    storyBox.style.display = "block";
+    photo.style.display = "block";
 
-
-// Story becomes visible
-    document.getElementById("story-box").style.display = "block";
+// Main box, "ready to play" header and button disappear
+    submit.style.display = "none";
+    mainBox.style.display = "none";
+    ready.style.display = "none";
 
 };
 
